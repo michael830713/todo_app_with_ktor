fe4c66eb5d928f496d6b5f572c84106ce68d5446
mike <michael830713@gmail.com>
2020-02-21 20:31:13 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/mikeleetech/todo_app_with_ktor/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mikeleetech.todo_app_with_ktor
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
