86d0bcb3764233f49cf8f7b896c78e60bd60c02c
mike <michael830713@gmail.com>
2020-04-21 15:12:24 +0800

    added app2 test

---
+++ test_driver/app2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:todo_app_with_ktor/main.dart';
+
+void main() {
+  // This line enables the extension.
+  enableFlutterDriverExtension();
+
+  // Call the `main()` function of the app, or call `runApp` with
+  // any widget you are interested in testing.
+  runApp(MyApp());
+}
